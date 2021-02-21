c2554df4ac05182939a9ce062f85498748f3962c
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-02-21 11:30:39 -0800

    add function to get manager's data

--- Index.js
+++ Index.js
@@ -12,13 +12,61 @@ let teamListArray = [];
 function userPrompt() {
     inquirer.prompt([
         {
+            name: "teamName",
             type: "input",
-            message: "What is your Team's Name?",
-            name: "teamName"
+            message: "Please enter your Team's name? (Required)",
+            validate: teamName => {
+                if (teamName) {
+                  return true;
+                } else {
+                  console.log("Please enter your Team's name!");
+                  return false;
+                }
+            }
         }
     ])
     .then(function(data) {
         const teamName = data.teamName;
         teamListArray.push(teamName);
+        addManager();
+    });
+};
+
+// add function to get manager's data
+function addManager() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please enter your Manager's name (Required)",
+            validate: managerName => {
+                if (managerName) {
+                  return true;
+                } else {
+                  console.log("Please enter your Manager's name!");
+                  return false;
+                }
+            }
+        },
+    
+        {
+            name: "email",
+            type: "input",
+            message: "Please enter your Manager's email address"
+        },
+
+        {
+            name: "officeNumber",
+            type: "number",
+            message: "Please enter your Manager's office number"
+        }
+    ])
+    .then(function(data) {
+        const name = name;
+        const id = 1;
+        const email = data.email;
+        const officeNumber = data.officeNumber;
+        const teamMember = new Manager(name, id, email, officeNumber);
+        teamListArray.push(teamMember);
     });
 };
